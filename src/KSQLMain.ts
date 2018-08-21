59bded05015b2d35175e0790bcf170a108b4dc6a
Didier Caron <dgcaron@gmail.com>
2018-08-22 00:10:14 +0200

    added describe methods to  enable describe methods from the explorer

--- src/KSQLMain.ts
+++ src/KSQLMain.ts
@@ -5,6 +5,7 @@ import * as http from 'typed-rest-client/HttpClient';
 import { KSQLConnectionConfig } from './KSQLConnectionConfig';
 import { KSQLClient } from './client/KSQLClient';
 import { KSQLExplorerProvider } from './explorer/KSQLExplorerProvider';
+import { KSQLTextDocumentContentProvider } from './preview/KSQLTextDocumentContentProvider';
 
 export let ksqlExplorerProvider: KSQLExplorerProvider;
 
@@ -15,9 +16,36 @@ export function activate(context: vscode.ExtensionContext) {
     let httpClient: http.HttpClient = new http.HttpClient("vs-code")
     let client: KSQLClient = new KSQLClient(httpClient, settings.url);
 
+    registerExplorer(context,client);
+    registerPreview(context,client);
+}
+
+function registerExplorer(context: vscode.ExtensionContext, client: KSQLClient){
     ksqlExplorerProvider = new KSQLExplorerProvider(client);
     vscode.window.registerTreeDataProvider('ksqlExplorer', ksqlExplorerProvider);
     vscode.commands.registerCommand('ksql.explorer.refresh', () => ksqlExplorerProvider.refresh());
+
+    vscode.commands.registerCommand('ksql.explorer.describe', (node) => {
+        return vscode.commands.executeCommand('vscode.previewHtml', vscode.Uri.parse("ksql:"+node.label+"?extended=false")).then((success) => {
+		}, (reason) => {
+			vscode.window.showErrorMessage(reason);
+		});
+	});
+
+    vscode.commands.registerCommand('ksql.explorer.describe_extended', (node) => {
+        return vscode.commands.executeCommand('vscode.previewHtml', vscode.Uri.parse("ksql:"+node.label+"?extended=true")).then((success) => {
+		}, (reason) => {
+			vscode.window.showErrorMessage(reason);
+		});
+	});
+ 
+}
+
+function registerPreview(context: vscode.ExtensionContext, client: KSQLClient){
+    let provider = new KSQLTextDocumentContentProvider(client);
+    let providerRegistrations = vscode.Disposable.from(vscode.workspace.registerTextDocumentContentProvider(KSQLTextDocumentContentProvider.scheme, provider))
+
+    context.subscriptions.push(providerRegistrations);
 }
 
 export function deactivate() {}
